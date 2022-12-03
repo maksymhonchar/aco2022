91da0ab4a29d5f46426b97fc7b0ef390e7df4d7d
Maxim Honchar Evo DELL PC <honchardev@gmail.com>
2022-12-03 23:45:58 +0200

    Add day3-part1 and day3-part2 solutions

---
+++ day_3/p1/solution.py
@@ -0,0 +1,21 @@
+def solution(
+    input_file_path: str
+) -> int:
+    priorities_sum = 0
+
+    with open(input_file_path) as fs_r:
+        for rucksack in fs_r:
+            rucksack_half = len(rucksack) // 2
+            shared_item = list(
+                set(rucksack[:rucksack_half]) & \
+                set(rucksack[rucksack_half:])
+            )[0]
+
+            priority_correction = \
+                1 - ord('a') if str.islower(shared_item) \
+                else 27 - ord('A')
+            shared_item_priority = ord(shared_item) + priority_correction
+
+            priorities_sum += shared_item_priority
+
+    return priorities_sum 
---
+++ day_3/p2/solution.py
@@ -0,0 +1,29 @@
+import operator
+
+def solution(
+    input_file_path: str
+) -> int:
+    priorities_sum = 0
+
+    with open(input_file_path) as fs_r:
+        for rucksack in fs_r:
+            group_rucksacks = (
+                set(group_rucksack.strip())
+                for group_rucksack in (
+                    rucksack,
+                    fs_r.__next__(),
+                    fs_r.__next__()
+                )
+            )
+            shared_item = list(
+                set.intersection(*group_rucksacks)
+            )[0]
+
+            priority_correction = \
+                1 - ord('a') if str.islower(shared_item) \
+                else 27 - ord('A')
+            shared_item_priority = ord(shared_item) + priority_correction
+
+            priorities_sum += shared_item_priority
+
+    return priorities_sum
