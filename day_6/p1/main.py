de7d6d4935d4889402694414381e7092181fe1f5
Maxim Honchar Evo DELL PC <honchardev@gmail.com>
2022-12-06 13:58:50 +0200

    Add day6-part1 and day6-part2 solutions

---
+++ day_6/p1/main.py
@@ -0,0 +1,12 @@
+from solution import solution
+
+
+def main():
+    input_file_path = 'day_6/inputs/test.in'
+
+    for result in solution(input_file_path):
+        print(f'{result=}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ day_6/p2/solution.py
@@ -0,0 +1,21 @@
+from typing import Generator, Optional
+
+
+def search(
+    datastream: str
+) -> Optional[int]:
+    marker_len = 14
+    for char_idx in range(len(datastream)):
+        marker = datastream[char_idx:char_idx+marker_len]
+        marker_chars_unique = len(set(marker)) == marker_len
+        if marker_chars_unique:
+            return char_idx + marker_len
+    return None
+
+
+def solution(
+    input_file_path: str
+) -> Generator[Optional[int], None, None]:
+    with open(input_file_path) as fs_r:
+        for datastream in fs_r:
+            yield search(datastream)
